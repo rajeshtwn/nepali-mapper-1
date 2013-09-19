417002aa00a16318e8dcdcf26b22a257483d1d1f
tensor <neuapneaadesh@gmail.com>
2013-09-20 00:31:06 +0545

    Adding rules for Nepali Mapper

---
+++ mapper.lisp
@@ -0,0 +1,89 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; @author:Aadesh Neupane
+;; @description: Maps the Nepali Word to the corresponding to the
+;;               rules described.
+;;
+;;
+
+;;Storing the Rules as Constants
+
+(defconstant ँ  'N'
+    "Chandra Bindu Matches to N.") 
+(defconstant ं  'M'
+    "...")
+(defconstant ा  'AA'
+    "...")
+(defconstant ि  'IH'
+    "...")
+(defconstant ी  'IH'
+    "...")
+(defconstant ु   'UH'
+    "...")    
+(defconstant ू   'UH'
+    "...")
+(defconstant े  'EH'
+    "...")
+(defconstant ै   "AH IH"
+    "...")    
+(defconstant ो  'OH'
+    "...")
+(defconstant ौ  "AH UH"
+    "...")
+(defconstant अ  'AH'
+    "...")
+(defconstant आ  'AA'
+    "...")
+(defconstant इ  'IH'
+    "...")
+(defconstant ई  'IH'
+    "...")
+(defconstant उ  'UH'
+    "...")
+(defconstant ऊ  'UH'
+    "...")
+(defconstant ऋ   "R IH"
+    "...")
+(defconstant ए  'EH'
+    "...")
+#|
+ऐ > AH IH
+ओ > OH
+औ > AH UN
+क > K AH
+ख > KA AH
+ग > G AH
+घ > GA AH
+ङ > NG AH
+च > CH AH
+छ > CHA AH
+ज > JH AH
+झ > JHA AH
+ञ > N AH
+ट > T AH
+ठ > TA AH
+ड > D AH
+ढ > DA AH
+ण > N AH
+त > TH AH
+थ > THA AH
+द > DH AH
+ध > DHA AH
+न > N AH
+प > P AH
+फ > PA AH
+ब > B AH
+भ > BA AH
+म > M AH
+य > Y AH
+र > R AH
+ल > L AH
+व > W AH
+स > S AH
+श > S AH
+ष > S AH
+ह > HH AH
+|#
+(print ं)
+(print ी)
+(print ै )
+
