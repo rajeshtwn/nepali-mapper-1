0783b52749ff2873a26481e9aa707a81e60a7dbd
Aadeshnpn <neupaneaadesh@gmail.com>
2013-09-25 21:56:39 +0545

    Tkinter Started

--- mapper.py
+++ mapper.py
@@ -5,6 +5,7 @@ Simple Rules to Map Nepali characters for Speech Processing
 """
 """Follow these guidelines to create the mapper by Wednesday(25th September)"""
 #Maps the argumets to the set of rules defined and return the mapped word 
+import Tkinter
 def map(word):
     pass
 
@@ -15,7 +16,8 @@ def rules():
 
 #Main Fuction that takes the file name as the argument and processes the input file to give the results
 def main():
-    pass
+    top=Tkinter.Tk()
+    top.mainloop()
 
 if __name__=='__main__':
     main()
